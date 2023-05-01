8285a6870f8bcf148a833d433ac88be579e3f8b4
Alisa Zhurova <zhurova2021@gmail.com>
2023-05-01 08:10:53 +0300

    feat: adding language switch

--- js/script.js
+++ js/script.js
@@ -1,68 +1,137 @@
-const KeyCodes = {
-  Backquote: '`',
-  Digit1: 1,
-  Digit2: 2,
-  Digit3: 3,
-  Digit4: 4,
-  Digit5: 5,
-  Digit6: 6,
-  Digit7: 7,
-  Digit8: 8,
-  Digit9: 9,
-  Digit0: 10,
-  Minus: '-',
-  Equal: '=',
-  Backspace: 'Backspace',
-  Tab: 'Tab',
-  KeyQ: 'q',
-  KeyW: 'w',
-  KeyE: 'e',
-  KeyR: 'r',
-  KeyT: 't',
-  KeyY: 'y',
-  KeyU: 'u',
-  KeyI: 'i',
-  KeyO: 'o',
-  KeyP: 'p',
-  BracketLeft: '[',
-  BracketRight: ']',
-  Backslash: '\\',
-  Delete: 'Delete',
-  CapsLock: 'CapsLock',
-  KeyA: 'a',
-  KeyS: 's',
-  KeyD: 'd',
-  KeyF: 'f',
-  KeyG: 'g',
-  KeyH: 'h',
-  KeyJ: 'j',
-  KeyK: 'k',
-  KeyL: 'l',
-  Semicolon: ';',
-  Quote: '\'',
-  Enter: 'Enter',
-  ShiftLeft: 'Shift',
-  KeyZ: 'z',
-  KeyX: 'x',
-  KeyC: 'c',
-  KeyV: 'v',
-  KeyB: 'b',
-  KeyN: 'n',
-  KeyM: 'm',
-  Comma: ',',
-  Period: '.',
-  Slash: '/',
-  ArrowUp: '▲',
-  ShiftRight: 'Shift',
-  ControlLeft: 'Ctrl',
-  Win: 'Win',
-  AltLeft: 'Alt',
-  Space: ' ',
-  AltRight: 'Alt',
-  ArrowLeft: '◄',
-  ArrowDown: '▼',
-  ArrowRight: '►',
-  ControlRight: 'Ctrl',
+let lang = 'en';
+const languages = {
+  en: {
+    Backquote: '`',
+    Digit1: 1,
+    Digit2: 2,
+    Digit3: 3,
+    Digit4: 4,
+    Digit5: 5,
+    Digit6: 6,
+    Digit7: 7,
+    Digit8: 8,
+    Digit9: 9,
+    Digit0: 0,
+    Minus: '-',
+    Equal: '=',
+    Backspace: 'Backspace',
+    Tab: 'Tab',
+    KeyQ: 'q',
+    KeyW: 'w',
+    KeyE: 'e',
+    KeyR: 'r',
+    KeyT: 't',
+    KeyY: 'y',
+    KeyU: 'u',
+    KeyI: 'i',
+    KeyO: 'o',
+    KeyP: 'p',
+    BracketLeft: '[',
+    BracketRight: ']',
+    Backslash: '\\',
+    Delete: 'Delete',
+    CapsLock: 'CapsLock',
+    KeyA: 'a',
+    KeyS: 's',
+    KeyD: 'd',
+    KeyF: 'f',
+    KeyG: 'g',
+    KeyH: 'h',
+    KeyJ: 'j',
+    KeyK: 'k',
+    KeyL: 'l',
+    Semicolon: ';',
+    Quote: '\'',
+    Enter: 'Enter',
+    ShiftLeft: 'Shift',
+    KeyZ: 'z',
+    KeyX: 'x',
+    KeyC: 'c',
+    KeyV: 'v',
+    KeyB: 'b',
+    KeyN: 'n',
+    KeyM: 'm',
+    Comma: ',',
+    Period: '.',
+    Slash: '/',
+    ArrowUp: '▲',
+    ShiftRight: 'Shift',
+    ControlLeft: 'Ctrl',
+    Win: 'Win',
+    AltLeft: 'Alt',
+    Space: ' ',
+    AltRight: 'Alt',
+    ArrowLeft: '◄',
+    ArrowDown: '▼',
+    ArrowRight: '►',
+    ControlRight: 'Ctrl',
+  },
+  ru: {
+    Backquote: 'ё',
+    Digit1: 1,
+    Digit2: 2,
+    Digit3: 3,
+    Digit4: 4,
+    Digit5: 5,
+    Digit6: 6,
+    Digit7: 7,
+    Digit8: 8,
+    Digit9: 9,
+    Digit0: 0,
+    Minus: '-',
+    Equal: '=',
+    Backspace: 'Backspace',
+    Tab: 'Tab',
+    KeyQ: 'й',
+    KeyW: 'ц',
+    KeyE: 'у',
+    KeyR: 'к',
+    KeyT: 'е',
+    KeyY: 'н',
+    KeyU: 'г',
+    KeyI: 'ш',
+    KeyO: 'щ',
+    KeyP: 'з',
+    BracketLeft: 'х',
+    BracketRight: 'ъ',
+    Backslash: '\\',
+    Delete: 'Delete',
+    CapsLock: 'CapsLock',
+    KeyA: 'ф',
+    KeyS: 'ы',
+    KeyD: 'в',
+    KeyF: 'а',
+    KeyG: 'п',
+    KeyH: 'р',
+    KeyJ: 'о',
+    KeyK: 'л',
+    KeyL: 'д',
+    Semicolon: 'ж',
+    Quote: 'э',
+    Enter: 'Enter',
+    ShiftLeft: 'Shift',
+    KeyZ: 'я',
+    KeyX: 'ч',
+    KeyC: 'с',
+    KeyV: 'м',
+    KeyB: 'и',
+    KeyN: 'т',
+    KeyM: 'ь',
+    Comma: 'б',
+    Period: 'ю',
+    Slash: '.',
+    ArrowUp: '▲',
+    ShiftRight: 'Shift',
+    ControlLeft: 'Ctrl',
+    Win: 'Win',
+    AltLeft: 'Alt',
+    Space: ' ',
+    AltRight: 'Alt',
+    ArrowLeft: '◄',
+    ArrowDown: '▼',
+    ArrowRight: '►',
+    ControlRight: 'Ctrl',
+  },
 };
 
 const container = document.querySelector('body');
@@ -80,45 +149,79 @@ const keyboard = document.createElement('div');
 keyboard.className = 'keyboard';
 document.body.append(keyboard);
 
-const keys = Object.keys(KeyCodes);
-const values = Object.values(KeyCodes);
-
 function init() {
+  const keys = Object.keys(languages[lang]);
+  const values = Object.values(languages[lang]);
   let out = '';
   for (let i = 0; i < keys.length; i += 1) {
     out += `<button class="keyboard__button button_${keys[i]}" data="${keys[i]}" >${values[i]}</button>`;
   }
   document.querySelector('.keyboard').innerHTML = out;
+
+  document.querySelectorAll('.keyboard .keyboard__button').forEach((element) => {
+    element.addEventListener('click', () => {
+      const code = element.getAttribute('data');
+      element.classList.add('active');
+      textarea.innerHTML += `${languages[lang][code]}`;
+    });
+  });
 }
 init();
 
 document.addEventListener('keydown', (event) => {
-  textarea.innerHTML += `${event.key}`;
+  const keys = Object.keys(languages[lang]);
   for (let i = 0; i < keys.length; i += 1) {
     document.querySelector(`.keyboard .keyboard__button[data=${event.code}]`).classList.add('active');
   }
+  if (lang === 'en') {
+    if (event.code === 'ControlLeft') {
+      document.onkeyup = (e) => {
+        if (e.code === 'AltLeft') {
+          lang = 'ru';
+          init();
+        }
+      };
+    }
+  } else if (lang === 'ru') {
+    if (event.code === 'ControlLeft') {
+      document.onkeyup = (e) => {
+        if (e.code === 'AltLeft') {
+          lang = 'en';
+          init();
+        }
+      };
+    }
+  }
+  textarea.innerHTML += `${languages[lang][event.code]}`;
 });
 
 document.addEventListener('keyup', () => {
+  const keys = Object.keys(languages[lang]);
   for (let i = 0; i < keys.length; i += 1) {
     document.querySelectorAll('.keyboard .keyboard__button').forEach((element) => {
       element.classList.remove('active');
     });
   }
-});
-
-document.querySelectorAll('.keyboard .keyboard__button').forEach((element) => {
-  element.addEventListener('click', () => {
-    const code = element.getAttribute('data');
-    element.classList.add('active');
-    textarea.innerHTML += `${KeyCodes[code]}`;
-  });
 });
 
 document.addEventListener('mouseout', () => {
+  const keys = Object.keys(languages[lang]);
   for (let i = 0; i < keys.length; i += 1) {
     document.querySelectorAll('.keyboard .keyboard__button').forEach((element) => {
       element.classList.remove('active');
     });
   }
 });
+
+function setLocalStorage() {
+  localStorage.setItem('lang', lang);
+}
+window.addEventListener('beforeunload', setLocalStorage);
+
+function getLocalStorage() {
+  if (localStorage.getItem('lang')) {
+    lang = localStorage.getItem('lang');
+    init();
+  }
+}
+window.addEventListener('load', getLocalStorage);
